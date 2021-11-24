4929a7f7ec14e73d96242451bf3fa2d1d7eab349
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-11-24 16:07:58 +0900

    Feat: redux + router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import ChatPage from "./components/ChatPage/ChatPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ChatPage />
+      <Routes>
+        <Route path="/" exact={true} element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RegisterPage() {
+  return <div>ResigerPage</div>;
+}
+
+export default RegisterPage;
