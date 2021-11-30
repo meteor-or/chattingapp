dd752452c5d3a5c4ee07d934c6f90cdd9ceda74e
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-11-30 23:55:05 +0900

    Feat: 파이어 베이트 챗팅 연동

--- src/components/ChatPage/Sidepanel/ChatRooms.js
+++ src/components/ChatPage/Sidepanel/ChatRooms.js
@@ -1,7 +1,160 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaRegSmileWink } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { Modal, Button, Form } from "react-bootstrap";
+import firebase from "../../../firebase";
+import {
+  getDatabase,
+  ref,
+  onChildAdded,
+  onValue,
+  push,
+  child,
+  update,
+  off,
+} from "firebase/database";
 
-function ChatRooms() {
-  return <div>ChatRooms</div>;
+import { connect } from "react-redux";
+
+export class ChatRooms extends Component {
+  state = {
+    show: false,
+    name: "",
+    description: "",
+    chatRoomsRef: ref(getDatabase(), "chatRooms"),
+    chatRooms: [],
+  };
+
+  componentDidMount() {
+    this.AddChatRoomsListeners();
+  }
+
+  AddChatRoomsListeners = () => {
+    let chatRoomsArray = [];
+
+    onChildAdded(this.state.chatRoomsRef, (DataSnapshot) => {
+      chatRoomsArray.push(DataSnapshot.val());
+      this.setState({ chatRooms: chatRoomsArray });
+    });
+  };
+
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, description } = this.state;
+
+    if (this.isFormValid(name, description)) {
+      this.addChatRoom();
+    }
+  };
+
+  addChatRoom = async () => {
+    const key = push(this.state.chatRoomsRef).key;
+    const { name, description } = this.state;
+    const { user } = this.props; //누가 방을 생성했는지
+    const newChatRoom = {
+      id: key,
+      name: name,
+      description: description,
+      createdBy: {
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+
+    try {
+      await update(child(this.state.chatRoomsRef, key), newChatRoom);
+      this.setState({
+        name: "",
+        description: "",
+        show: false,
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  isFormValid = (name, description) => name && description;
+
+  renderChatRooms = (chatRooms) =>
+    chatRooms.length > 0 &&
+    chatRooms.map((room) => <li key={room.id}>#{room.name}</li>);
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <FaRegSmileWink style={{ marginRight: 3 }} />
+          CHAT ROOMS (1)
+          <button style={{ backgroundColor: "transparent" }}>
+            <FaPlus
+              onClick={this.handleShow}
+              style={{
+                color: "white",
+              }}
+            />
+          </button>
+        </div>
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderChatRooms(this.state.chatRooms)}
+        </ul>
+        {/* 챗룸 추가모달창 */}{" "}
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>채팅방 만들기</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>방 이름</Form.Label>
+                <Form.Control
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                  type="text"
+                  placeholder="채팅방 이름을 입력하세요"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>방 설명</Form.Label>
+                <Form.Control
+                  onChange={(e) =>
+                    this.setState({ description: e.target.value })
+                  }
+                  type="text"
+                  placeholder="채팅 방 설명을 입력하세요"
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              닫기
+            </Button>
+            <Button variant="primary" onClick={this.handleSubmit}>
+              방 만들기
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-export default ChatRooms;
+//스테이트에 들어있는 것을 프랍스로 바꿔서 사용하겠다는 것을 알려줌
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(ChatRooms);
