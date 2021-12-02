9bd34e2316a08dfa5229f76bcc60dc0fe320ae4f
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-12-02 23:34:43 +0900

    Feat: message show

--- src/components/ChatPage/Mainpanel/Mainpanel.js
+++ src/components/ChatPage/Mainpanel/Mainpanel.js
@@ -1,13 +1,59 @@
 import React, { Component } from "react";
-import MassageHeader from "./MassageHeader";
-import Massage from "./Massage";
-import MasseageForm from "./MasseageForm";
+import MessageHeader from "./MessageHeader";
+import Message from "./Message";
+import MesseageForm from "./MesseageForm";
+import { connect } from "react-redux";
+import {
+  getDatabase,
+  ref,
+  onChildAdded,
+  onChildRemoved,
+  child,
+  off,
+} from "firebase/database";
 
 export class Mainpanel extends Component {
+  state = {
+    messages: [],
+    messagesRef: ref(getDatabase(), "messages"),
+    messagesLoading: true,
+  };
+
+  componentDidMount() {
+    const { chatRoom } = this.props;
+
+    if (chatRoom) {
+      this.addMessagesListeners(chatRoom.id);
+    }
+  }
+  addMessagesListeners = (chatRoomId) => {
+    let messagesArray = [];
+    let { messagesRef } = this.state;
+
+    this.state.messagesRef
+      .child(chatRoomId)
+      .on("child_added", (DataSnapshot) => {
+        messagesArray.push(DataSnapshot.val());
+        this.setState({ messages: messagesArray, messagesLoading: false });
+      });
+  };
+
+  renderMessages = (messages) =>
+    messages.length > 0 &&
+    //컴포넌트 하나를 더 추가함 message
+    messages.map((message) => (
+      <Message
+        key={messages.timestamp}
+        messages={message}
+        user={this.props.user}
+      />
+    ));
+
   render() {
+    const { messages } = this.state;
     return (
       <div style={{ padding: "2rem 2rem 0 2rem" }}>
-        <MassageHeader />
+        <MessageHeader />
         <div
           style={{
             width: "100%",
@@ -18,11 +64,20 @@ export class Mainpanel extends Component {
             marginBottom: "1rem",
             overflow: "auto",
           }}
-        ></div>
-        <MasseageForm />
+        >
+          {this.renderMessages(messages)}
+        </div>
+        <MesseageForm />
       </div>
     );
   }
 }
 
-export default Mainpanel;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currntUser,
+    chatRoom: state.chatRoom.currentChatRoom,
+  };
+};
+
+export default connect(mapStateToProps)(Mainpanel);
--- src/components/ChatPage/Mainpanel/Massage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Massage() {
-  return <div>Massage</div>;
-}
-
-export default Massage;
