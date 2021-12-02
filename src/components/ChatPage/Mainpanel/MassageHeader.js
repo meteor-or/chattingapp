6923103096d3a43cf0cfba39e481eff8d423c02b
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-12-02 22:58:36 +0900

    Feat: message save

--- src/components/ChatPage/Mainpanel/MassageHeader.js
+++ src/components/ChatPage/Mainpanel/MassageHeader.js
@@ -1,7 +1,93 @@
 import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Image,
+  Accordion,
+} from "react-bootstrap";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaLock } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
 
 function MassageHeader() {
-  return <div>MassageHeader</div>;
+  return (
+    <header
+      style={{
+        width: "100%",
+        height: "170px",
+        border: ".1rem solid #ececec",
+        borderRadius: "4px",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>
+            <h2>
+              <FaLock /> ChatRoomName <MdFavorite />
+            </h2>
+          </Col>
+          <Col>
+            {" "}
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <AiOutlineSearch />
+              </InputGroup.Text>
+
+              <FormControl
+                placeholder="메세지 검색하기"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "flex-end" }}>
+          <Image scr="" /> user name
+        </Row>
+        <Row>
+          <Col>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Description</Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+          <Col>
+            <Accordion>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header></Accordion.Header>
+                <Accordion.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 
 export default MassageHeader;
--- src/components/ChatPage/Mainpanel/MasseageForm.js
+++ src/components/ChatPage/Mainpanel/MasseageForm.js
@@ -1,7 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, ProgressBar, Row, Col } from "react-bootstrap";
+import firebase from "../../../firebase";
+import { useSelector } from "react-redux";
+import { getDatabase, ref, set, remove, push, child } from "firebase/database";
 
 function MasseageForm() {
-  return <div>MasseageForm</div>;
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+  const [content, setContent] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState("");
+  const messagesRef = ref(getDatabase(), "messages");
+
+  const createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: new Date(),
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+    // 파일이 있으면 이미지를 보내고 파일이 없으면 컨텐츠를 보내기
+    if (fileUrl !== null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = content;
+    }
+
+    return message;
+  };
+  //키보드를 입력할 때마다 컨텐츠를 바꿔준다.
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    if (!content) {
+      setErrors((prev) => prev.concat("Type contents first"));
+      return;
+    }
+    setLoading(true);
+    //firebase에 메시지를 저장하는 부분
+    try {
+      await set(push(child(messagesRef, chatRoom.id)), createMessage());
+      setLoading(false);
+      setContent("");
+      setErrors([]);
+    } catch (error) {
+      setErrors((pre) => pre.concat(error.message));
+      setLoading(false);
+      setTimeout(() => {
+        setErrors([]);
+      }, 5000);
+    }
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control
+            value={content}
+            onChange={handleChange}
+            as="textarea"
+            rows={3}
+          />
+        </Form.Group>
+      </Form>
+
+      <ProgressBar variant="warning" label="60%" now={60} />
+      <div>
+        {errors.map((errorMsg) => (
+          <p style={{ color: "red" }} key={errorMsg}>
+            {errorMsg}
+          </p>
+        ))}
+      </div>
+
+      <Row>
+        <Col>
+          <button
+            onClick={handleSubmit}
+            className="message_form_btn"
+            style={{ width: "100%" }}
+          >
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message_form_btn" style={{ width: "100%" }}>
+            upload
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MasseageForm;
